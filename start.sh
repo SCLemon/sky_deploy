21d5201acb621bfc317a837bd618b5b8797e59b8
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-09-22 20:16:07 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# 啟動 https.js
-echo "Starting https.js..."
-nohup node ./https.js > ./logs/https.log 2>&1 &
+# 啟動 https.js 背景程式
+nohup node ./https.js >/dev/null 2>&1 &
 
-# 啟動 backend/index.js
-echo "Starting backend/index.js..."
-nohup node ./backend/index.js > ./logs/backend.log 2>&1 &
+# 啟動 backend/index.js 背景程式
+nohup node ./backend/index.js >/dev/null 2>&1 &
 
 echo "All services started."
