35be9c128af5d96edc2b1d33361d0761381938de
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-10-19 20:50:33 +0800

    config

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,13 @@
-//配置文件
-module.exports = {
-  DBHOST: '127.0.0.1',
-  DBPORT: 27017,
-  DBNAME: 'SkyAcademy'
-}
-
-// docker network
+// 一般環境配置文件
 // module.exports = {
-//   DBHOST: 'sky_mongo',
+//   DBHOST: '127.0.0.1',
 //   DBPORT: 27017,
 //   DBNAME: 'SkyAcademy'
 // }
+
+// docker network
+module.exports = {
+  DBHOST: 'sky_mongo',
+  DBPORT: 27017,
+  DBNAME: 'SkyAcademy'
+}
