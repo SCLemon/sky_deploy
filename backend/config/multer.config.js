aaadde146be827c17b4c0ee2c5bed699e6d0809b
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-11-10 22:26:27 +0800

    debug

--- backend/config/multer.config.js
+++ backend/config/multer.config.js
@@ -1,17 +1,27 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const os = require('os');
 
 
-const uploadDir = path.join(os.tmpdir(), 'sky_uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
+// 跨平台基準路徑
+let tmpDir;
+if (process.platform === 'win32') {
+    tmpDir = 'D:/sky_database/sky_tmp';
+} 
+else if (process.platform === 'darwin') {
+    tmpDir = '/Volumes/sky_database/sky_tmp';
+} 
+else {
+    tmpDir = '/mnt/sky_database/sky_tmp';
+}
+
+if (!fs.existsSync(tmpDir)) {
+  fs.mkdirSync(tmpDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, tmpDir);
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -20,7 +30,7 @@ const storage = multer.diskStorage({
 });
 
 
-// 限制檔案大小 (例如 10MB)
+// 限制檔案大小
 const upload = multer({
   storage,
 //   limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
